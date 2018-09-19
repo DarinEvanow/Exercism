8770646a67bf65e6f304eec7aa00035c2a6bdddf
Darin <darinevanow@gmail.com>
2018-09-19 14:34:54 -0700

    Big overhaul of the markdown parser. Includes easier regex to account for each of the different markdown cases (strong, lists, em, and header), and process functions for each case.

--- elixir/markdown/markdown.exs
+++ elixir/markdown/markdown.exs
@@ -10,83 +10,34 @@ defmodule Markdown do
     iex> Markdown.parse("#Header!\n* __Bold Item__\n* _Italic Item_")
     "<h1>Header!</h1><ul><li><em>Bold Item</em></li><li><i>Italic Item</i></li></ul>"
   """
-  # Refactor: Spread out nested function calls into pipes
+  # Refactor: Spread out nested function calls into pipes, changed map and join into a map_join,
+  # call new versions of the process function, and replace patch with replace_md
   @spec parse(String.t()) :: String.t()
   def parse(markdown) do
     markdown
     |> String.split("\n")
-    |> Enum.map(fn t -> process(t) end)
-    |> Enum.join
-    |> patch
+    |> Enum.map_join(fn line -> process(line) end)
+    |> replace_markdown
   end
 
-  # Refactor: Changed nested if logic into cond
-  defp process(t) do
-    cond do
-      String.starts_with?(t, "#") -> enclose_with_header_tag(parse_header_md_level(t))
-      String.starts_with?(t, "*") -> parse_list_md_level(t)
-      true -> enclose_with_paragraph_tag(String.split(t))
-    end
+  # Refactor: Created new versions of process function that are called based on the markdown
+  # element that begins the line split from the parse function
+  @spec process(String.t()) :: String.t()
+  defp process(line = "#" <> rest), do: parse_header(line)
+  defp process("* " <> rest), do: "<li>#{rest}</li>"
+  defp process(line), do: "<p>#{line}</p>"
+
+  # Refactor: Created new function to parse the header type of markdown
+  defp parse_header(header) do
+    [ h | t ] = String.split(header, " ", parts: 2)
+    "<h#{String.length(h)}>#{t}</h#{String.length(h)}>"
   end
 
-  defp parse_header_md_level(header) do
-    [h | t] = String.split(header)
-    {to_string(String.length(h)), Enum.join(t, " ")}
-  end
-
-  # Refactor: Piped out functions, and broke string concatenation into enclose_with_last_tag
-  defp parse_list_md_level(list) do
-    String.trim_leading(list, "* ")
-    |> String.split
-    |> join_words_with_tags
-    |> enclose_with_list_tag
-  end
-
-  # Refactor: Additionally added helper method
-  defp enclose_with_list_tag(text) do
-    "<li>#{text}</li>"
-  end
-
-  # Refactor: combination of string interpolation and concatenation to increase readability
-  defp enclose_with_header_tag({hl, htl}) do
-    "<h#{hl}>" <> "#{htl}" <> "</h#{hl}>"
-  end
-
-  defp enclose_with_paragraph_tag(text) do
-    "<p>#{join_words_with_tags(text)}</p>"
-  end
-
-  defp join_words_with_tags(text) do
-    Enum.map(text, fn word -> replace_md_with_tag(word) end)
-    |> Enum.join(" ")
-  end
-
-  defp replace_md_with_tag(markdown) do
+  # Refactor: Created easier regex to help parse out the different markdown types
+  defp replace_markdown(markdown) do
     markdown
-    |> replace_prefix_md
-    |> replace_suffix_md
-  end
-
-  defp replace_prefix_md(word) do
-    cond do
-      word =~ ~r/^#{"__"}{1}/ -> String.replace(word, ~r/^#{"__"}{1}/, "<strong>", global: false)
-      word =~ ~r/^[#{"_"}{1}][^#{"_"}+]/ -> String.replace(word, ~r/_/, "<em>", global: false)
-      true -> word
-    end
-  end
-
-  defp replace_suffix_md(word) do
-    cond do
-      word =~ ~r/#{"__"}{1}$/ -> String.replace(word, ~r/#{"__"}{1}$/, "</strong>")
-      word =~ ~r/[^#{"_"}{1}]/ -> String.replace(word, ~r/_/, "</em>")
-      true -> word
-    end
-  end
-
-  # Refactor: used piping and removed unneeded concatenation operators
-  defp patch(markdown) do
-    markdown
-    |> String.replace("<li>", "<ul><li>", global: false)
-    |> String.replace_suffix("</li>", "</li></ul>")
+    |> String.replace(~r/__(.*?)__/, "<strong>\\1</strong>")
+    |> String.replace(~r/_(.*?)_/, "<em>\\1</em>")
+    |> String.replace(~r/<li>.*<\/li>/, "<ul>\\0</ul>")
   end
 end
